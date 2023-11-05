1f5d90a3615ab4c1d5dc7983f703a9e68a5b3e95
Barry Hoggard <bhoggard@gmail.com>
2023-11-05 15:08:40 -0500

    add year to artworks table

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,17 @@
 class Artwork < ApplicationRecord
+  validates :title, :medium, :location, :year, presence: true
+  validates :units, inclusion: {in: %w[in cm],
+                                message: "%{value} is not a valid unit"}
+
+  validate :dimensions
+
   belongs_to :user
+
+  private
+
+  def dimensions
+    if duration.blank? && (height.blank? || width.blank?)
+      errors.add(:either, "Height and Width or Duration is required")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_05_013704) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_05_200511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_05_013704) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "year", null: false
     t.index ["user_id"], name: "index_artworks_on_user_id"
+    t.index ["year"], name: "index_artworks_on_year"
   end
 
   create_table "users", force: :cascade do |t|
