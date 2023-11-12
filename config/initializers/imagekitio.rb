3cadfb3aeb4e64896c2055f8223b3507c562faf1
Barry Hoggard <bhoggard@gmail.com>
2023-11-12 18:50:54 -0500

    fix ImageKitIo config

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,9 +1,6 @@
 ImageKitIo.configure do |config|
-  if Rails.env.development?
-    config.public_key = ENV.fetch("IMAGEKIT_PUBLIC_KEY")
-    config.private_key = ENV.fetch("IMAGEKIT_PRIVATE_KEY")
-    config.url_endpoint = ENV.fetch("IMAGEKIT_URL")
-  end
+  config.public_key = ENV.fetch("IMAGEKIT_PUBLIC_KEY")
+  config.private_key = ENV.fetch("IMAGEKIT_PRIVATE_KEY")
+  config.url_endpoint = ENV.fetch("IMAGEKIT_URL")
   config.service = :active_storage
-  # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
 end
