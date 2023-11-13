9ccb4a3d345331edaffc07211968714797112caf
Barry Hoggard <bhoggard@gmail.com>
2023-11-12 20:18:08 -0500

    added edition field, plus instructions on required fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_12_234353) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_014508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,12 +58,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_234353) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "year", null: false
+    t.string "edition", default: "", null: false
     t.index ["user_id"], name: "index_artworks_on_user_id"
     t.index ["year"], name: "index_artworks_on_year"
   end
 
   create_table "images", force: :cascade do |t|
-    t.string "caption"
+    t.string "caption", default: ""
     t.bigint "artwork_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
