29e27ca23a7c3b6c8442fce47a73f29705c62059
Barry Hoggard <bhoggard@gmail.com>
2023-11-28 23:57:48 -0500

    add chargebee customer id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_040102) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_26_024327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,7 +91,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_040102) do
     t.boolean "active", default: true, null: false
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
+    t.string "cb_customer_id"
     t.index ["active"], name: "index_users_on_active"
+    t.index ["cb_customer_id"], name: "index_users_on_cb_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["last_name", "first_name"], name: "index_users_on_last_name_and_first_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
