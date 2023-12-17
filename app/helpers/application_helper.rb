290c59b30b540bff26d56313caadc2afc5cf86a4
Barry Hoggard <bhoggard@gmail.com>
2023-12-17 18:35:24 -0500

    draft of image layout on artwork pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       image.file.url(transformation: [{height: 100, quality: 100, raw: "c-at_max"}])
     end
   end
+
+  def large_url(image)
+    if image.is_video?
+      "#{image.file.url}/ik-thumbnail.jpg?tr=h-600"
+    else
+      image.file.url(transformation: [{height: 400, quality: 100, raw: "c-at_max"}])
+    end
+  end
 end
