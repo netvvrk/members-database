a5b008938c1793bcfde1279db16a21bf45aa62e8
Barry Hoggard <bhoggard@gmail.com>
2023-12-17 19:40:31 -0500

    update artwork images displays and editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,17 @@ module ApplicationHelper
 
   def large_url(image)
     if image.is_video?
-      "#{image.file.url}/ik-thumbnail.jpg?tr=h-600"
+      "#{image.file.url}/ik-thumbnail.jpg?tr=h-400"
     else
       image.file.url(transformation: [{height: 400, quality: 100, raw: "c-at_max"}])
+    end
+  end
+
+  def asset_delete_text(i)
+    if i.is_video?
+      "Delete video"
+    else
+      "Delete image"
     end
   end
 end
