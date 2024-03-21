5a23b9f1dd0fa0d7ec29ee13a6cbf05de2a59adb
Barry Hoggard <bhoggard@gmail.com>
2024-03-21 00:41:53 -0400

    added file type and size validation for file uploads

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,32 @@
 class Profile < ApplicationRecord
-    belongs_to :user
-    has_one_attached :avatar
-    has_one_attached :cv
+  belongs_to :user
+  has_one_attached :avatar
+  has_one_attached :cv
+
+  validate :cv_size_and_format
+  validate :avatar_size_and_format
+
+  private
+
+  def cv_size_and_format
+    if cv.attached?
+      if cv.content_type != "application/pdf"
+        errors.add(:cv, "Must be a PDF")
+      end
+      if cv.byte_size > 25.megabytes
+        errors.add(:cv, "Must be under 25MB")
+      end
+    end
+  end
+
+  def avatar_size_and_format
+    if avatar.attached?
+      if !avatar.content_type.match?(/^image\//)
+        errors.add(:avatar, "Must be an image")
+      end
+      if avatar.byte_size > 25.megabytes
+        errors.add(:avatar, "Must be under 25MB")
+      end
+    end
+  end
 end
