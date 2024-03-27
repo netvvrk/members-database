506cfef2dfbd33e90b0924b5846c4200d3e3a6e6
sepans <sepans@sepans.com>
2024-03-27 11:04:22 -0400

    Adding the null checks to image/video helpers
    It is not necessary but it doesn't harm either. And it is helpful while developing when testing artworks without images...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def thumbnail_url(image)
-    if image.is_video?
+    if image&.is_video?
       "#{image.file.url}/ik-thumbnail.jpg?tr=h-100"
     else
-      image.file.url(transformation: [{height: 100, width: 100, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
+      image&.file&.url(transformation: [{height: 100, width: 100, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
     end
   end
 
   def large_url(image)
-    if image.is_video?
+    if image&.is_video?
       "#{image.file.url}/ik-thumbnail.jpg?tr=h-400,w=400,c-maintain_ratio"
     else
-      image.file.url(transformation: [{height: 400, width: 400, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
+      image&.file&.url(transformation: [{height: 400, width: 400, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
     end
   end
 
