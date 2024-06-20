32c71b3f8d51c8252435e18d815d88887f5b62aa
Barry Hoggard <bhoggard@gmail.com>
2024-06-19 21:42:35 -0400

    subscription created works

--- app/lib/webhook_handler.rb
+++ app/lib/webhook_handler.rb
@@ -1,3 +1,5 @@
+require "securerandom"
+
 class WebhookHandler
   class << self
     def handle_payload(payload)
@@ -5,14 +7,30 @@ class WebhookHandler
 
       case data["event_type"]
       when "subscription_created"
-        handle_subscription(payload)
+        handle_subscription(data)
       else
         false
       end
     end
 
     def handle_subscription(payload)
-      true
+      customer = payload["content"]["customer"]
+      password = SecureRandom.hex
+
+      u = User.create(
+        email: customer["email"],
+        first_name: customer["first_name"],
+        last_name: customer["last_name"],
+        role: "artist",
+        password: password,
+        password_confirmation: password
+      )
+      if u.valid?
+        u.send_reset_password_instructions
+        true
+      else
+        Rails.logger.error("User creation for #{customer["id"]} failed -- #{u.errors.messages}")
+      end
     end
   end
 end
--- test/lib/webhook_handler_test.rb
+++ test/lib/webhook_handler_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class WebhookHandlerTest < ActiveSupport::TestCase
   test "subscription created" do
     payload = File.read(Rails.root.join("test", "fixtures", "files", "subscription_created.json"))
-    assert WebhookHandler.handle_payload(payload)
+    assert_difference "User.count" do
+      assert WebhookHandler.handle_payload(payload)
+    end
   end
 end
