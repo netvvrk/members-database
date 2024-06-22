107289db1e50bc8196cb66739a34d1c142220afb
Barry Hoggard <bhoggard@gmail.com>
2024-06-22 14:59:56 -0400

    create accounts for founding members when syncing

--- app/lib/util.rb
+++ app/lib/util.rb
@@ -1,8 +1,8 @@
 class Util
   class << self
-    def subscription_is_annual(subscription)
+    def subscription_is_annual_or_founding(subscription)
       subscription_types = subscription.subscription_items.map(&:item_price_id)
-      subscription_types.any? { |t| t =~ /Annual/ }
+      subscription_types.any? { |t| t =~ /Annual|Founding/ }
     end
   end
 end
