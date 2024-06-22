c5764f7c64577bb4df74b15e7099cceff0f2b8a5
Barry Hoggard <bhoggard@gmail.com>
2024-06-22 13:40:50 -0400

    only create users for annual subscribers

--- app/lib/chargebee_sync.rb
+++ app/lib/chargebee_sync.rb
@@ -9,23 +9,9 @@ class ChargebeeSync
     loop do
       resp = ChargeBee::Subscription.list(search_condition)
       resp.each do |entry|
-        customer = entry.customer
-        cb_id = customer.id
-
-        next if User.exists?(cb_customer_id: cb_id)
-        puts "ID: #{cb_id}"
-
-        password = SecureRandom.alphanumeric(12)
-        u = User.create!(
-          cb_customer_id: cb_id,
-          email: customer.email,
-          first_name: customer.first_name,
-          last_name: customer.last_name,
-          password: password,
-          password_confirmation: password,
-          role: "artist"
-        )
-        u.send_reset_password_instructions
+        print "."
+        next unless Util.subscription_is_annual(entry.subscription)
+        create_user(entry.customer)
       end
       if resp.next_offset
         search_condition[:offset] = resp.next_offset
@@ -33,5 +19,23 @@ class ChargebeeSync
         break
       end
     end
+    puts "end"
+  end
+
+  def create_user(customer)
+    cb_id = customer.id
+    return if User.exists?(cb_customer_id: cb_id)
+
+    password = SecureRandom.alphanumeric(12)
+    u = User.create!(
+      cb_customer_id: cb_id,
+      email: customer.email,
+      first_name: customer.first_name,
+      last_name: customer.last_name,
+      password: password,
+      password_confirmation: password,
+      role: "artist"
+    )
+    u.send_reset_password_instructions
   end
 end
