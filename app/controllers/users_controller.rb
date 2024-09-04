98136835c42f33f2935873200f974cdb20796735
Barry Hoggard <bhoggard@gmail.com>
2024-09-03 20:48:25 -0400

    added admin update of user password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,30 +23,30 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
 
-    if @user.save
-      redirect_to @user, notice: "User was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  #   if @user.save
+  #     redirect_to @user, notice: "User was successfully created."
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: "User was successfully updated.", status: :see_other
+      redirect_to users_path, notice: "User was successfully updated.", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy!
-    redirect_to users_url, notice: "User was successfully destroyed.", status: :see_other
-  end
+  # def destroy
+  #   @user.destroy!
+  #   redirect_to users_url, notice: "User was successfully destroyed.", status: :see_other
+  # end
 
   def login_as
     bypass_sign_in(@user)
@@ -62,7 +62,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:email, :name, :role)
+    params.require(:user).permit(:password, :password_confirmation)
   end
 
   def confirm_admin
