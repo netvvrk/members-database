2d02fa77d196a510d576a901ea3a0acdee316370
Barry Hoggard <bhoggard@gmail.com>
2024-12-17 22:00:09 -0500

    add more rescue errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,9 @@ module ApplicationHelper
     else
       image&.file&.url(transformation: [{height: 100, width: 100, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
     end
+  rescue => e
+    Rails.logger.error("thumbnail_url failed for Image ID #{image.id}: #{e}")
+    ""
   end
 
   def large_url(image)
@@ -13,6 +16,9 @@ module ApplicationHelper
     else
       image&.file&.url(transformation: [{height: 400, width: 400, quality: 100, raw: "c-at_max,c-maintain_ratio"}])
     end
+  rescue => e
+    Rails.logger.error("large_url failed for Image ID #{image.id}: #{e}")
+    ""
   end
 
   # image with 400px height and extra gray padding to maintain ratio
