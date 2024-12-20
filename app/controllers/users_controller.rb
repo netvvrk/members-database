d3c8e1eb3bb651deac251226d94ed3a4f6497e17
Barry Hoggard <bhoggard@gmail.com>
2024-12-19 20:16:19 -0500

    Admins can add new users (#121)
    * WIP
    
    * yarn install
    
    * allow admins to create users
    
    ---------
    
    Co-authored-by: Barry Hoggard <barry@hoggard.org>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,16 +22,25 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users
-  # def create
-  #   @user = User.new(user_params)
+  # GET /users/new
+  def new
+    @user = User.new
+  end
 
-  #   if @user.save
-  #     redirect_to @user, notice: "User was successfully created."
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  # POST /users
+  def create
+    name = user_params[:name]
+    params = user_params.except(:name)
+    @user = User.new(params)
+
+    if @user.save
+      @user.profile.update_attribute(:name, name)
+      redirect_to users_path, notice: "User was successfully created."
+    else
+      session[:user_name] = name
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   # PATCH/PUT /users/1
   def update
@@ -68,7 +77,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
   end
 
   def confirm_admin
