1a4eaaea3f752148ac12549ce68fdd76c656099f
Barry Hoggard <bhoggard@gmail.com>
2025-02-04 20:44:22 -0500

    don't crash on non-numeric price queries

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,4 +1,13 @@
 require "test_helper"
 
 class MainControllerTest < ActionDispatch::IntegrationTest
+  test "get index" do
+    get root_url
+    assert_response :success
+  end
+
+  test "get index with non-numeric price query does not crash" do
+    get root_url, params: {min_price: 100, max_price: "test"}
+    assert_response :success
+  end
 end
